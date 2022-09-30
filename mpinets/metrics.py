376b183c4df33d3ab2aa88b86abb0bde3f20ef23
Adam Fishman <afishman@nvidia.com>
2022-09-30 13:23:12 -0700

    Moved sparc computation into separate file

--- mpinets/metrics.py
+++ mpinets/metrics.py
@@ -37,6 +37,7 @@ from mpinets.types import Obstacles
 from geometrout.primitive import Cuboid, Sphere, Cylinder
 from typing import Sequence, Union, List, Tuple, Any, Dict, Optional
 from mpinets.types import Obstacles, Trajectory
+from mpinets.third_party.sparc import sparc
 
 try:
     import lula
@@ -55,88 +56,6 @@ def percent_true(arr: Sequence) -> float:
     :rtype float: The percent
     """
     return 100 * np.count_nonzero(arr) / len(arr)
-
-
-def sparc(movement, fs, padlevel=4, fc=10.0, amp_th=0.05):
-    """
-    Calculates the smoothness of the given speed profile using the modified
-    spectral arc length metric.
-    Parameters
-    ----------
-    movement : np.array
-               The array containing the movement speed profile.
-    fs       : float
-               The sampling frequency of the data.
-    padlevel : integer, optional
-               Indicates the amount of zero padding to be done to the movement
-               data for estimating the spectral arc length. [default = 4]
-    fc       : float, optional
-               The max. cut off frequency for calculating the spectral arc
-               length metric. [default = 10.]
-    amp_th   : float, optional
-               The amplitude threshold to used for determing the cut off
-               frequency upto which the spectral arc length is to be estimated.
-               [default = 0.05]
-    Returns
-    -------
-    sal      : float
-               The spectral arc length estimate of the given movement's
-               smoothness.
-    (f, Mf)  : tuple of two np.arrays
-               This is the frequency(f) and the magntiude spectrum(Mf) of the
-               given movement data. This spectral is from 0. to fs/2.
-    (f_sel, Mf_sel) : tuple of two np.arrays
-                      This is the portion of the spectrum that is selected for
-                      calculating the spectral arc length.
-    Notes
-    -----
-    This is the modfieid spectral arc length metric, which has been tested only
-    for discrete movements.
-    Code comes from https://github.com/siva82kb/SPARC/blob/master/scripts/smoothness.py
-    Examples
-    --------
-    >>> t = np.arange(-1, 1, 0.01)
-    >>> move = np.exp(-5*pow(t, 2))
-    >>> sal, _, _ = sparc(move, fs=100.)
-    >>> '%.5f' % sal
-    '-1.41403'
-    """
-    if np.allclose(movement, 0):
-        print("All movement was 0, returning 0")
-        return 0, None, None
-    # Number of zeros to be padded.
-    nfft = int(pow(2, np.ceil(np.log2(len(movement))) + padlevel))
-
-    # Frequency
-    f = np.arange(0, fs, fs / nfft)
-    # Normalized magnitude spectrum
-    Mf = abs(np.fft.fft(movement, nfft))
-    Mf = Mf / max(Mf)
-
-    # Indices to choose only the spectrum within the given cut off frequency
-    # Fc.
-    # NOTE: This is a low pass filtering operation to get rid of high frequency
-    # noise from affecting the next step (amplitude threshold based cut off for
-    # arc length calculation).
-    fc_inx = ((f <= fc) * 1).nonzero()
-    f_sel = f[fc_inx]
-    Mf_sel = Mf[fc_inx]
-
-    # Choose the amplitude threshold based cut off frequency.
-    # Index of the last point on the magnitude spectrum that is greater than
-    # or equal to the amplitude threshold.
-    inx = ((Mf_sel >= amp_th) * 1).nonzero()[0]
-    fc_inx = range(inx[0], inx[-1] + 1)
-    f_sel = f_sel[fc_inx]
-    Mf_sel = Mf_sel[fc_inx]
-
-    # Calculate arc length
-    new_sal = -sum(
-        np.sqrt(
-            pow(np.diff(f_sel) / (f_sel[-1] - f_sel[0]), 2) + pow(np.diff(Mf_sel), 2)
-        )
-    )
-    return new_sal, (f, Mf), (f_sel, Mf_sel)
 
 
 class Evaluator:
---
+++ mpinets/third_party/sparc.py
@@ -0,0 +1,100 @@
+# Copyright (c) 2015, Sivakumar Balasubramanian <siva82kb@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+# Note: original code can be found on Github at
+# https://github.com/siva82kb/SPARC/blob/master/scripts/smoothness.py
+
+import numpy as np
+
+
+def sparc(movement, fs, padlevel=4, fc=10.0, amp_th=0.05):
+    """
+    Calculates the smoothness of the given speed profile using the modified
+    spectral arc length metric.
+    Parameters
+    ----------
+    movement : np.array
+               The array containing the movement speed profile.
+    fs       : float
+               The sampling frequency of the data.
+    padlevel : integer, optional
+               Indicates the amount of zero padding to be done to the movement
+               data for estimating the spectral arc length. [default = 4]
+    fc       : float, optional
+               The max. cut off frequency for calculating the spectral arc
+               length metric. [default = 10.]
+    amp_th   : float, optional
+               The amplitude threshold to used for determing the cut off
+               frequency upto which the spectral arc length is to be estimated.
+               [default = 0.05]
+    Returns
+    -------
+    sal      : float
+               The spectral arc length estimate of the given movement's
+               smoothness.
+    (f, Mf)  : tuple of two np.arrays
+               This is the frequency(f) and the magntiude spectrum(Mf) of the
+               given movement data. This spectral is from 0. to fs/2.
+    (f_sel, Mf_sel) : tuple of two np.arrays
+                      This is the portion of the spectrum that is selected for
+                      calculating the spectral arc length.
+    Notes
+    -----
+    This is the modfieid spectral arc length metric, which has been tested only
+    for discrete movements.
+
+    Examples
+    --------
+    >>> t = np.arange(-1, 1, 0.01)
+    >>> move = np.exp(-5*pow(t, 2))
+    >>> sal, _, _ = sparc(move, fs=100.)
+    >>> '%.5f' % sal
+    '-1.41403'
+    """
+    if np.allclose(movement, 0):
+        print("All movement was 0, returning 0")
+        return 0, None, None
+    # Number of zeros to be padded.
+    nfft = int(pow(2, np.ceil(np.log2(len(movement))) + padlevel))
+
+    # Frequency
+    f = np.arange(0, fs, fs / nfft)
+    # Normalized magnitude spectrum
+    Mf = abs(np.fft.fft(movement, nfft))
+    Mf = Mf / max(Mf)
+
+    # Indices to choose only the spectrum within the given cut off frequency
+    # Fc.
+    # NOTE: This is a low pass filtering operation to get rid of high frequency
+    # noise from affecting the next step (amplitude threshold based cut off for
+    # arc length calculation).
+    fc_inx = ((f <= fc) * 1).nonzero()
+    f_sel = f[fc_inx]
+    Mf_sel = Mf[fc_inx]
+
+    # Choose the amplitude threshold based cut off frequency.
+    # Index of the last point on the magnitude spectrum that is greater than
+    # or equal to the amplitude threshold.
+    inx = ((Mf_sel >= amp_th) * 1).nonzero()[0]
+    fc_inx = range(inx[0], inx[-1] + 1)
+    f_sel = f_sel[fc_inx]
+    Mf_sel = Mf_sel[fc_inx]
+
+    # Calculate arc length
+    new_sal = -sum(
+        np.sqrt(
+            pow(np.diff(f_sel) / (f_sel[-1] - f_sel[0]), 2) + pow(np.diff(Mf_sel), 2)
+        )
+    )
+    return new_sal, (f, Mf), (f_sel, Mf_sel)
